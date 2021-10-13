4a921df498a73e1f7967f32d8ae84e164329bd86
Mukesh Solanki <91374635+BotMasterOfficial@users.noreply.github.com>
2021-10-13 06:59:37 +0530

    Update mongo.py

--- ZaidRobot/mongo.py
+++ ZaidRobot/mongo.py
@@ -1,4 +1,4 @@
-#Kiitu
+# Mukesh Solanki
 import asyncio
 import sys
 
@@ -11,7 +11,7 @@ from ZaidRobot.conf import get_int_key, get_str_key
 
 MONGO_PORT = get_int_key("27017")
 MONGO_DB_URI = get_str_key("MONGO_DB_URI")
-MONGO_DB = "DaisyX"
+MONGO_DB = "ZaidRobot"
 
 
 client = MongoClient()
