32d55206f231cadacc6eea54532fac30a005b624
Mukesh Solanki <91374635+BotMasterOfficial@users.noreply.github.com>
2021-10-13 06:41:19 +0530

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,2 @@
+worker: python3 -m ZaidRobot
+ps:scale worker=1
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+TITLE Zaid Robot
+:: Enables virtual env mode and then starts Shasa
+env\scripts\activate.bat && py -m ZaidRobot
